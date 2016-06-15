096aae41a86ea683e20b384337f65a447ecc3175
Pavel Myagkov <p.myagkov@corp.mail.ru>
2016-06-15 22:11:17 +0300

    Fix tests

--- test/insertCssSpec.js
+++ test/insertCssSpec.js
@@ -11,6 +11,7 @@ import jsdom from 'jsdom';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import insertCss from '../src/insertCss';
+import md5 from 'js-md5';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.parentWindow;
@@ -18,27 +19,15 @@ global.window = document.parentWindow;
 describe('insertCss(styles, options)', () => {
   it('Should insert and remove <style> element', () => {
     const css = 'body { color: red; }';
-    const removeCss = insertCss([[1, css]]);
-    let style = global.document.getElementById('s1-0');
+    const md5sum = md5(css);
+
+    const removeCss = insertCss([[1, css]], { md5: md5sum });
+    let style = global.document.getElementById(md5sum);
     expect(style).to.be.ok;
     expect(style.textContent).to.be.equal(css);
     expect(removeCss).to.be.func;
     removeCss();
-    style = global.document.getElementById('s1-0');
+    style = global.document.getElementById(md5sum);
     expect(style).to.be.null;
-  });
-
-  it('Should insert and remove multiple <style> elements for a single module', () => {
-    const css1 = 'body { color: red; }';
-    const css2 = 'body { color: blue; }';
-    const removeCss = insertCss([[1, css1], [1, css2]]);
-    let style = global.document.getElementsByTagName('style');
-    expect(style.length).to.be.equal(2);
-    expect(style[0].textContent).to.be.equal(css1);
-    expect(style[1].textContent).to.be.equal(css2);
-    expect(removeCss).to.be.func;
-    removeCss();
-    style = global.document.getElementsByTagName('style');
-    expect(style.length).to.be.equal(0);
   });
 });
